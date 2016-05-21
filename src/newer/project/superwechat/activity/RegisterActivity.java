1f444881fb6ecbb5bacddcdef1fc557846c4aead
Newer <403353881@qq.com>
2016-05-21 10:18:59 +0800

    完善RegisterActivity类的功能，并添加注释

--- src/newer/project/superwechat/activity/RegisterActivity.java
+++ src/newer/project/superwechat/activity/RegisterActivity.java
@@ -26,10 +26,16 @@ import com.easemob.EMError;
 import com.easemob.chat.EMChatManager;
 import com.easemob.exceptions.EaseMobException;
 
+import java.io.File;
+
 import newer.project.superwechat.I;
 import newer.project.superwechat.R;
 import newer.project.superwechat.SuperWeChatApplication;
+import newer.project.superwechat.bean.Message;
 import newer.project.superwechat.listener.OnSetAvatarListener;
+import newer.project.superwechat.utils.ImageUtils;
+import newer.project.superwechat.utils.OkHttpUtils2;
+import newer.project.superwechat.utils.Utils;
 
 /**
  * 注册页
@@ -41,36 +47,47 @@ public class RegisterActivity extends BaseActivity {
 	private EditText passwordEditText;
 	private EditText confirmPwdEditText;
 	private ImageView updateAvatarImageView;
-	private String avatarName;
+	private String avatarName;//暂时保存的头像名，用于提交注册请求
 	private OnSetAvatarListener mOnSetAvatarListener;
+	private final static String TAG = RegisterActivity.class.getName();
+	private static String SERVER_ROOT = "http://10.0.2.2:8080/SuperWeChatServer/Server";
+	ProgressDialog pd;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_register);
+		initView();
+		setListener();
+	}
+
+	/** 实体化布局属性 */
+	private void initView() {
 		userNameEditText = (EditText) findViewById(R.id.username);
 		nickEditText = (EditText) findViewById(R.id.nick);
 		passwordEditText = (EditText) findViewById(R.id.password);
 		confirmPwdEditText = (EditText) findViewById(R.id.confirm_password);
 		updateAvatarImageView = (ImageView) findViewById(R.id.iv_updateAvatar);
-		setListener();
 	}
 
+	/** 设置监听器 */
 	private void setListener() {
 		setOnLoginListener();
 		setOnRegisterListener();
 		setAvatarListener();
 	}
 
+	/** 点击选取头像布局，给两个选取头像方法的按钮设置选取头像监听器 */
 	private void setAvatarListener() {
 		findViewById(R.id.layout_updateAvatar).setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View view) {
-				mOnSetAvatarListener = new OnSetAvatarListener(RegisterActivity.this, R.id.layout_register, getAvatarName(), I.AVATAR_TYPE_USER_PATH);
+				mOnSetAvatarListener = new OnSetAvatarListener(RegisterActivity.this, R.id.layout_register, getAvatarName(), I.AVATAR_TYPE_USER_PATH);//在OnSetAvatarListener类中自动为两个按钮设置监听
 			}
 		});
 	}
 
+	/** 设置注册按钮监听器 */
 	private void setOnRegisterListener() {
 		findViewById(R.id.btn_lRegister_register).setOnClickListener(new View.OnClickListener() {
 			@Override
@@ -80,6 +97,7 @@ public class RegisterActivity extends BaseActivity {
 		});
 	}
 
+	/** 设置登录按钮监听器，返回登录界面 */
 	private void setOnLoginListener() {
 		findViewById(R.id.btn_lRegister_login).setOnClickListener(new View.OnClickListener() {
 			@Override
@@ -89,25 +107,22 @@ public class RegisterActivity extends BaseActivity {
 		});
 	}
 
+	/** 设置暂存头像名，并返回 */
 	public String getAvatarName() {
+		avatarName = String.valueOf(System.currentTimeMillis());
 		return avatarName;
 	}
 
-	public void setAvatarName(String avatarName) {
-		this.avatarName = avatarName;
-	}
-
+	/** 接收mOnSetAvatarListener获得头像的信息，将头像显示到updateAvatarImageView上 */
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		super.onActivityResult(requestCode, resultCode, data);
-		if (requestCode == RESULT_OK) {
-			mOnSetAvatarListener.setAvatar(requestCode,data,updateAvatarImageView);
+		if (resultCode == RESULT_OK) {
+			mOnSetAvatarListener.setAvatar(requestCode, data, updateAvatarImageView);
 		}
 	}
 
-	/**
-	 * 注册
-	 */
+	/** 注册 */
 	private void register() {
 		final String username = userNameEditText.getText().toString().trim();
 		final String nick = nickEditText.getText().toString().trim();
@@ -138,51 +153,110 @@ public class RegisterActivity extends BaseActivity {
 		}
 
 		if (!TextUtils.isEmpty(username) && !TextUtils.isEmpty(pwd)) {
-			final ProgressDialog pd = new ProgressDialog(this);
+			pd = new ProgressDialog(this);
 			pd.setMessage(getResources().getString(R.string.Is_the_registered));
 			pd.show();
-
-			new Thread(new Runnable() {
-				public void run() {
-					try {
-						// 调用sdk注册方法
-						EMChatManager.getInstance().createAccountOnServer(username, pwd);
-						runOnUiThread(new Runnable() {
-							public void run() {
-								if (!RegisterActivity.this.isFinishing())
-									pd.dismiss();
-								// 保存用户名
-								SuperWeChatApplication.getInstance().setUserName(username);
-								Toast.makeText(getApplicationContext(), getResources().getString(R.string.Registered_successfully), 0).show();
-								finish();
-							}
-						});
-					} catch (final EaseMobException e) {
-						runOnUiThread(new Runnable() {
-							public void run() {
-								if (!RegisterActivity.this.isFinishing())
-									pd.dismiss();
-								int errorCode=e.getErrorCode();
-								if(errorCode==EMError.NONETWORK_ERROR){
-									Toast.makeText(getApplicationContext(), getResources().getString(R.string.network_anomalies), Toast.LENGTH_SHORT).show();
-								}else if(errorCode == EMError.USER_ALREADY_EXISTS){
-									Toast.makeText(getApplicationContext(), getResources().getString(R.string.User_already_exists), Toast.LENGTH_SHORT).show();
-								}else if(errorCode == EMError.UNAUTHORIZED){
-									Toast.makeText(getApplicationContext(), getResources().getString(R.string.registration_failed_without_permission), Toast.LENGTH_SHORT).show();
-								}else if(errorCode == EMError.ILLEGAL_USER_NAME){
-								    Toast.makeText(getApplicationContext(), getResources().getString(R.string.illegal_user_name),Toast.LENGTH_SHORT).show();
-								}else{
-									Toast.makeText(getApplicationContext(), getResources().getString(R.string.Registration_failed) + e.getMessage(), Toast.LENGTH_SHORT).show();
-								}
-							}
-						});
-					}
-				}
-			}).start();
-
+			registerServer(username, nick, pwd);
 		}
 	}
 
+	/** 注册到本地服务端 */
+	private void registerServer(final String username, final String nick, final String pwd) {
+		File file = new File(ImageUtils.getAvatarPath(RegisterActivity.this, I.AVATAR_TYPE_USER_PATH), avatarName + I.AVATAR_SUFFIX_JPG);
+		OkHttpUtils2<Message> utils = new OkHttpUtils2<Message>();
+		utils.url(SERVER_ROOT)
+				.addParam(I.KEY_REQUEST,I.REQUEST_REGISTER)
+				.addParam(I.User.USER_NAME, username)
+				.addParam(I.User.NICK, nick)
+				.addParam(I.User.PASSWORD, pwd)
+				.addFile(file)
+				.targetClass(Message.class)
+				.execute(new OkHttpUtils2.OnCompleteListener<Message>() {
+					@Override
+					public void onSuccess(Message result) {
+						if (result.isResult()) {
+							registerEMServer(username,nick,pwd);
+						} else {
+							Utils.showToast(RegisterActivity.this,Utils.getResourceString(RegisterActivity.this,result.getMsg()),Toast.LENGTH_SHORT);
+							pd.dismiss();
+						}
+					}
+
+					@Override
+					public void onError(String error) {
+						Utils.showToast(RegisterActivity.this,error,Toast.LENGTH_SHORT);
+						pd.dismiss();
+					}
+				});
+
+	}
+
+	/** 注册到环信服务端 */
+	private void registerEMServer(final String username, final String nick, final String pwd) {
+		new Thread(new Runnable() {
+			public void run() {
+				try {
+					// 调用sdk注册方法
+					EMChatManager.getInstance().createAccountOnServer(username, pwd);
+					runOnUiThread(new Runnable() {
+						public void run() {
+							if (!RegisterActivity.this.isFinishing())
+								pd.dismiss();
+							// 保存用户名
+							SuperWeChatApplication.getInstance().setUserName(username);
+							Toast.makeText(getApplicationContext(), getResources().getString(R.string.Registered_successfully), 0).show();
+							finish();
+						}
+					});
+				} catch (final EaseMobException e) {
+					unregister(username);
+					runOnUiThread(new Runnable() {
+						public void run() {
+							if (!RegisterActivity.this.isFinishing())
+								pd.dismiss();
+							int errorCode=e.getErrorCode();
+							if(errorCode== EMError.NONETWORK_ERROR){
+								Toast.makeText(getApplicationContext(), getResources().getString(R.string.network_anomalies), Toast.LENGTH_SHORT).show();
+							}else if(errorCode == EMError.USER_ALREADY_EXISTS){
+								Toast.makeText(getApplicationContext(), getResources().getString(R.string.User_already_exists), Toast.LENGTH_SHORT).show();
+							}else if(errorCode == EMError.UNAUTHORIZED){
+								Toast.makeText(getApplicationContext(), getResources().getString(R.string.registration_failed_without_permission), Toast.LENGTH_SHORT).show();
+							}else if(errorCode == EMError.ILLEGAL_USER_NAME){
+								Toast.makeText(getApplicationContext(), getResources().getString(R.string.illegal_user_name),Toast.LENGTH_SHORT).show();
+							}else{
+								Toast.makeText(getApplicationContext(), getResources().getString(R.string.Registration_failed) + e.getMessage(), Toast.LENGTH_SHORT).show();
+							}
+						}
+					});
+				}
+			}
+		}).start();
+	}
+
+	/** 环信服务端注册失败时，取消本地服务端的注册 */
+	private void unregister(String username) {
+		OkHttpUtils2<Message> utils = new OkHttpUtils2<Message>();
+		utils.url(SERVER_ROOT)
+				.addParam(I.KEY_REQUEST, I.REQUEST_UNREGISTER)
+				.addParam(I.User.USER_NAME, username)
+				.targetClass(Message.class)
+				.execute(new OkHttpUtils2.OnCompleteListener<Message>() {
+					@Override
+					public void onSuccess(Message result) {
+						if (!result.isResult()) {
+							Utils.showToast(RegisterActivity.this,Utils.getResourceString(RegisterActivity.this,result.getMsg()),Toast.LENGTH_SHORT);
+						}
+					}
+
+					@Override
+					public void onError(String error) {
+						Utils.showToast(RegisterActivity.this,error,Toast.LENGTH_SHORT);
+						pd.dismiss();
+					}
+				});
+	}
+
+	/** 返回登录界面 */
 	private void back() {
 		finish();
 	}
