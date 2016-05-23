45288efbf66f08d640a9e209186bd6a26fb3b4db
Newer <403353881@qq.com>
2016-05-23 10:21:35 +0800

    task包中创建了DownloadPublicGroupTask类，添加intiPath和execute方法

---
+++ src/newer/project/superwechat/task/DownloadPublicGroupTask.java
@@ -0,0 +1,63 @@
+package newer.project.superwechat.task;
+
+import android.content.Context;
+import android.content.Intent;
+
+import com.android.volley.Response;
+
+import java.util.ArrayList;
+
+import newer.project.superwechat.I;
+import newer.project.superwechat.SuperWeChatApplication;
+import newer.project.superwechat.activity.BaseActivity;
+import newer.project.superwechat.bean.Group;
+import newer.project.superwechat.data.ApiParams;
+import newer.project.superwechat.data.GsonRequest;
+import newer.project.superwechat.utils.Utils;
+
+public class DownloadPublicGroupTask extends BaseActivity {
+    private static final String TAG = DownloadContactListTask.class.toString();
+    private Context context;
+    private String username;
+    private int pageId;
+    private int pageSize;
+    private String path;
+
+    public DownloadPublicGroupTask(Context context, String username, int pageId, int pageSize) {
+        this.context = context;
+        this.username = username;
+        this.pageId = pageId;
+        this.pageSize = pageSize;
+    }
+
+    public void initPath() {
+        try {
+            path = new ApiParams()
+                    .with(I.User.USER_NAME, username)
+                    .with(I.PAGE_ID, String.valueOf(pageId))
+                    .with(I.PAGE_SIZE, String.valueOf(pageSize))
+                    .getRequestUrl(I.REQUEST_FIND_PUBLIC_GROUPS);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void execute() {
+        executeRequest(new GsonRequest<Group[]>(path,Group[].class,responseDownloadPublicGroupTask(),errorListener()));
+    }
+
+    private Response.Listener<Group[]> responseDownloadPublicGroupTask() {
+        return new Response.Listener<Group[]>() {
+            @Override
+            public void onResponse(Group[] contacts) {
+                if (contacts != null) {
+                    ArrayList<Group> publicGroupList = SuperWeChatApplication.getInstance().getPublicGroupList();
+                    ArrayList<Group> list = Utils.array2List(contacts);
+                    publicGroupList.clear();
+                    publicGroupList.addAll(list);
+                    sendStickyBroadcast(new Intent("update_public_group_list"));
+                }
+            }
+        };
+    }
+}
