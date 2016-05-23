c8181c704f688d78a8902d61650f34710703d342
Newer <403353881@qq.com>
2016-05-23 17:37:21 +0800

    utils包的UserUtils类中添加setUserBeanNick方法

--- src/newer/project/superwechat/utils/UserUtils.java
+++ src/newer/project/superwechat/utils/UserUtils.java
@@ -2,20 +2,21 @@ package newer.project.superwechat.utils;
 
 import android.content.Context;
 import android.text.TextUtils;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.android.volley.toolbox.NetworkImageView;
+import com.squareup.picasso.Picasso;
+
+import newer.project.superwechat.DemoHXSDKHelper;
 import newer.project.superwechat.I;
+import newer.project.superwechat.R;
 import newer.project.superwechat.SuperWeChatApplication;
 import newer.project.superwechat.applib.controller.HXSDKHelper;
-import newer.project.superwechat.DemoHXSDKHelper;
-import newer.project.superwechat.R;
 import newer.project.superwechat.bean.Contact;
 import newer.project.superwechat.data.RequestManager;
 import newer.project.superwechat.domain.EMUser;
-
-import com.android.volley.toolbox.NetworkImageView;
-import com.squareup.picasso.Picasso;
 
 public class UserUtils {
     /**
@@ -38,6 +39,7 @@ public class UserUtils {
     }
 
 	public static Contact getContactInfo(String username) {
+
 		return SuperWeChatApplication.getInstance().getUserList().get(username);
 	}
 
@@ -55,8 +57,13 @@ public class UserUtils {
     }
 
 	public static void setContactAvatar(String username, NetworkImageView niv) {
+		Log.i("my", username);
 		Contact contact = getContactInfo(username);
+		Log.i("my", "contact" + contact);
 		if (contact != null && contact.getMContactUserName() != null) {
+			String s = getAvatarPath(username);
+			Log.i("my", s);
+			setUserAvatar(s, niv);
 			setUserAvatar(getAvatarPath(username), niv);
 		}
 	}
@@ -100,7 +107,16 @@ public class UserUtils {
     		textView.setText(username);
     	}
     }
-    
+
+	public static void setUserBeanNick(String username,TextView textView){
+		Contact contact = getContactInfo(username);
+		if(contact != null && contact.getMUserNick() != null){
+			textView.setText(contact.getMUserNick());
+		}else{
+			textView.setText(username);
+		}
+	}
+
     /**
      * 设置当前用户昵称
      */
@@ -113,7 +129,7 @@ public class UserUtils {
     
     /**
      * 保存或更新某个用户
-     * @param user
+     * @param newUser
      */
 	public static void saveUserInfo(EMUser newUser) {
 		if (newUser == null || newUser.getUsername() == null) {
