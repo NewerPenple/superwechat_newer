abdde1f006f705d40858ffafc0cee003ba547917
Newer <403353881@qq.com>
2016-05-24 16:51:50 +0800

    GroupPickContactsActivity类中修改onCreate方法中的allUserList属性类型及相关代码，修改PickContactAdapter内部类

--- src/newer/project/superwechat/activity/GroupPickContactsActivity.java
+++ src/newer/project/superwechat/activity/GroupPickContactsActivity.java
@@ -13,11 +13,6 @@
  */
 package newer.project.superwechat.activity;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
-
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -30,14 +25,19 @@ import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.ListView;
 
-import newer.project.superwechat.applib.controller.HXSDKHelper;
 import com.easemob.chat.EMGroup;
 import com.easemob.chat.EMGroupManager;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 import newer.project.superwechat.Constant;
-import newer.project.superwechat.DemoHXSDKHelper;
 import newer.project.superwechat.R;
+import newer.project.superwechat.SuperWeChatApplication;
 import newer.project.superwechat.adapter.ContactAdapter;
-import newer.project.superwechat.domain.User;
+import newer.project.superwechat.bean.Contact;
 import newer.project.superwechat.widget.Sidebar;
 
 public class GroupPickContactsActivity extends BaseActivity {
@@ -67,16 +67,16 @@ public class GroupPickContactsActivity extends BaseActivity {
 		if(exitingMembers == null)
 			exitingMembers = new ArrayList<String>();
 		// 获取好友列表
-		final List<User> alluserList = new ArrayList<User>();
-		for (User user : ((DemoHXSDKHelper)HXSDKHelper.getInstance()).getContactList().values()) {
-			if (!user.getUsername().equals(Constant.NEW_FRIENDS_USERNAME) & !user.getUsername().equals(Constant.GROUP_USERNAME) & !user.getUsername().equals(Constant.CHAT_ROOM) & !user.getUsername().equals(Constant.CHAT_ROBOT))
+		final ArrayList<Contact> alluserList = new ArrayList<Contact>();
+		for (Contact user : SuperWeChatApplication.getInstance().getUserList().values()) {
+			if (!user.getMContactCname().equals(Constant.NEW_FRIENDS_USERNAME) & !user.getMContactCname().equals(Constant.GROUP_USERNAME) & !user.getMContactCname().equals(Constant.CHAT_ROOM) & !user.getMContactCname().equals(Constant.CHAT_ROBOT))
 				alluserList.add(user);
 		}
 		// 对list进行排序
-		Collections.sort(alluserList, new Comparator<User>() {
+		Collections.sort(alluserList, new Comparator<Contact>() {
 			@Override
-			public int compare(User lhs, User rhs) {
-				return (lhs.getUsername().compareTo(rhs.getUsername()));
+			public int compare(Contact lhs, Contact rhs) {
+				return (lhs.getHeader().compareTo(rhs.getHeader()));
 
 			}
 		});
@@ -115,7 +115,7 @@ public class GroupPickContactsActivity extends BaseActivity {
 		List<String> members = new ArrayList<String>();
 		int length = contactAdapter.isCheckedArray.length;
 		for (int i = 0; i < length; i++) {
-			String username = contactAdapter.getItem(i).getUsername();
+			String username = contactAdapter.getItem(i).getMContactCname();
 			if (contactAdapter.isCheckedArray[i] && !exitingMembers.contains(username)) {
 				members.add(username);
 			}
@@ -131,7 +131,7 @@ public class GroupPickContactsActivity extends BaseActivity {
 
 		private boolean[] isCheckedArray;
 
-		public PickContactAdapter(Context context, int resource, List<User> users) {
+		public PickContactAdapter(Context context, int resource, ArrayList<Contact> users) {
 			super(context, resource, users);
 			isCheckedArray = new boolean[users.size()];
 		}
@@ -140,7 +140,7 @@ public class GroupPickContactsActivity extends BaseActivity {
 		public View getView(final int position, View convertView, ViewGroup parent) {
 			View view = super.getView(position, convertView, parent);
 //			if (position > 0) {
-				final String username = getItem(position).getUsername();
+				final String username = getItem(position).getMContactCname();
 				// 选择框checkbox
 				final CheckBox checkBox = (CheckBox) view.findViewById(R.id.checkbox);
 				if(exitingMembers != null && exitingMembers.contains(username)){
