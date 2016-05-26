c92c5ed8333a9bc1e840642ccb4315e47bc490f0
Newer <403353881@qq.com>
2016-05-26 16:40:48 +0800

    NewGroupActivity类中定义mOnSetAvatarListener和avatarName属性，提取并创建initView和setListener方法，initView方法中实体化mLayoutUploadGroupAvatar，mivUploadGroupAvatar，mtvUploadGroupAvatar属性，setListener方法中给mLayoutUploadGroupAvatar设置监听器，添加getAvatarName方法，在onActivityResult方法中响应mOnSetAvatarListener信息

--- src/newer/project/superwechat/activity/NewGroupActivity.java
+++ src/newer/project/superwechat/activity/NewGroupActivity.java
@@ -22,12 +22,18 @@ import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.easemob.chat.EMGroupManager;
-import newer.project.superwechat.R;
 import com.easemob.exceptions.EaseMobException;
+
+import newer.project.superwechat.I;
+import newer.project.superwechat.R;
+import newer.project.superwechat.listener.OnSetAvatarListener;
 
 public class NewGroupActivity extends BaseActivity {
 	private EditText groupNameEditText;
@@ -36,28 +42,54 @@ public class NewGroupActivity extends BaseActivity {
 	private CheckBox checkBox;
 	private CheckBox memberCheckbox;
 	private LinearLayout openInviteContainer;
+	private RelativeLayout mLayoutUploadGroupAvatar;
+	private ImageView mivUploadGroupAvatar;
+	private TextView mtvUploadGroupAvatar;
+	private OnSetAvatarListener mOnSetAvatarListener;
+	private String avatarName;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_new_group);
+		initView();
+		setListener();
+	}
+
+	private void setListener() {
+		checkBox.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				if (isChecked) {
+					openInviteContainer.setVisibility(View.INVISIBLE);
+				} else {
+					openInviteContainer.setVisibility(View.VISIBLE);
+				}
+			}
+		});
+		mLayoutUploadGroupAvatar.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				mOnSetAvatarListener = new OnSetAvatarListener(NewGroupActivity.this, R.id.layout_new_group, getAvatarName(), I.AVATAR_TYPE_GROUP_PATH);
+			}
+		});
+	}
+
+	public String getAvatarName() {
+		avatarName = String.valueOf(System.currentTimeMillis());
+		return avatarName;
+	}
+
+	private void initView() {
 		groupNameEditText = (EditText) findViewById(R.id.edit_group_name);
 		introductionEditText = (EditText) findViewById(R.id.edit_group_introduction);
 		checkBox = (CheckBox) findViewById(R.id.cb_public);
 		memberCheckbox = (CheckBox) findViewById(R.id.cb_member_inviter);
 		openInviteContainer = (LinearLayout) findViewById(R.id.ll_open_invite);
-		
-		checkBox.setOnCheckedChangeListener(new OnCheckedChangeListener() {
-			
-			@Override
-			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-				if(isChecked){
-					openInviteContainer.setVisibility(View.INVISIBLE);
-				}else{
-					openInviteContainer.setVisibility(View.VISIBLE);
-				}
-			}
-		});
+		mLayoutUploadGroupAvatar = (RelativeLayout) findViewById(R.id.layout_uploadGroupAvatar);
+		mivUploadGroupAvatar = (ImageView) findViewById(R.id.iv_uploadGroupAvatar);
+		mtvUploadGroupAvatar = (TextView) findViewById(R.id.tv_uploadGroupAvatar);
 	}
 
 	/**
@@ -83,46 +115,55 @@ public class NewGroupActivity extends BaseActivity {
 		final String st2 = getResources().getString(R.string.Failed_to_create_groups);
 		if (resultCode == RESULT_OK) {
 			//新建群组
-			progressDialog = new ProgressDialog(this);
-			progressDialog.setMessage(st1);
-			progressDialog.setCanceledOnTouchOutside(false);
-			progressDialog.show();
+			setProgressDialog(st1);
+			createNewGroup(data, st2);
 
-			new Thread(new Runnable() {
-				@Override
-				public void run() {
-					// 调用sdk创建群组方法
-					String groupName = groupNameEditText.getText().toString().trim();
-					String desc = introductionEditText.getText().toString();
-					String[] members = data.getStringArrayExtra("newmembers");
-					try {
-						if(checkBox.isChecked()){
-							//创建公开群，此种方式创建的群，可以自由加入
-							//创建公开群，此种方式创建的群，用户需要申请，等群主同意后才能加入此群
-						    EMGroupManager.getInstance().createPublicGroup(groupName, desc, members, true,200);
-						}else{
-							//创建不公开群
-						    EMGroupManager.getInstance().createPrivateGroup(groupName, desc, members, memberCheckbox.isChecked(),200);
-						}
-						runOnUiThread(new Runnable() {
-							public void run() {
-								progressDialog.dismiss();
-								setResult(RESULT_OK);
-								finish();
-							}
-						});
-					} catch (final EaseMobException e) {
-						runOnUiThread(new Runnable() {
-							public void run() {
-								progressDialog.dismiss();
-								Toast.makeText(NewGroupActivity.this, st2 + e.getLocalizedMessage(), 1).show();
-							}
-						});
-					}
-					
-				}
-			}).start();
 		}
+		mOnSetAvatarListener.setAvatar(requestCode, data, mivUploadGroupAvatar);
+	}
+
+	private void createNewGroup(final Intent data, final String st2) {
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				// 调用sdk创建群组方法
+				String groupName = groupNameEditText.getText().toString().trim();
+				String desc = introductionEditText.getText().toString();
+				String[] members = data.getStringArrayExtra("newmembers");
+				try {
+					if(checkBox.isChecked()){
+						//创建公开群，此种方式创建的群，可以自由加入
+						//创建公开群，此种方式创建的群，用户需要申请，等群主同意后才能加入此群
+						EMGroupManager.getInstance().createPublicGroup(groupName, desc, members, true,200);
+					}else{
+						//创建不公开群
+						EMGroupManager.getInstance().createPrivateGroup(groupName, desc, members, memberCheckbox.isChecked(),200);
+					}
+					runOnUiThread(new Runnable() {
+						public void run() {
+							progressDialog.dismiss();
+							setResult(RESULT_OK);
+							finish();
+						}
+					});
+				} catch (final EaseMobException e) {
+					runOnUiThread(new Runnable() {
+						public void run() {
+							progressDialog.dismiss();
+							Toast.makeText(NewGroupActivity.this, st2 + e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+						}
+					});
+				}
+
+			}
+		}).start();
+	}
+
+	public void setProgressDialog(String st1) {
+		progressDialog = new ProgressDialog(this);
+		progressDialog.setMessage(st1);
+		progressDialog.setCanceledOnTouchOutside(false);
+		progressDialog.show();
 	}
 
 	public void back(View view) {
