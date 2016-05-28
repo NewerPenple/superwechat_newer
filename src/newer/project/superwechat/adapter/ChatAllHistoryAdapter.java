94f3701e97dc91c5d9da9706a96d5353662ac1a1
Newer <403353881@qq.com>
2016-05-28 20:59:41 +0800

    ChatAllHistoryAdapter类的getView方法中修改显示群头像和名称代码

--- src/newer/project/superwechat/adapter/ChatAllHistoryAdapter.java
+++ src/newer/project/superwechat/adapter/ChatAllHistoryAdapter.java
@@ -43,8 +43,11 @@ import java.util.Map;
 
 import newer.project.superwechat.Constant;
 import newer.project.superwechat.DemoHXSDKHelper;
+import newer.project.superwechat.I;
 import newer.project.superwechat.R;
 import newer.project.superwechat.applib.controller.HXSDKHelper;
+import newer.project.superwechat.bean.Group;
+import newer.project.superwechat.data.RequestManager;
 import newer.project.superwechat.domain.RobotUser;
 import newer.project.superwechat.utils.DateUtils;
 import newer.project.superwechat.utils.SmileUtils;
@@ -100,9 +103,12 @@ public class ChatAllHistoryAdapter extends ArrayAdapter<EMConversation> {
 		String username = conversation.getUserName();
 		if (conversation.getType() == EMConversationType.GroupChat) {
 			// 群聊消息，显示群聊头像
-			holder.avatar.setImageResource(R.drawable.group_icon);
-			EMGroup group = EMGroupManager.getInstance().getGroup(username);
-			holder.name.setText(group != null ? group.getGroupName() : username);
+			String avatarUrl = I.REQUEST_DOWNLOAD_GROUP_AVATAR_URL + username;
+			holder.avatar.setDefaultImageResId(R.drawable.group_icon);
+			holder.avatar.setImageUrl(avatarUrl, RequestManager.getImageLoader());
+			holder.avatar.setErrorImageResId(R.drawable.group_icon);
+			Group group = UserUtils.getGroupByHXID(username);
+			holder.name.setText(group != null ? group.getMGroupName() : username);
 		} else if(conversation.getType() == EMConversationType.ChatRoom){
 		    holder.avatar.setImageResource(R.drawable.group_icon);
             EMChatRoom room = EMChatManager.getInstance().getChatRoom(username);
